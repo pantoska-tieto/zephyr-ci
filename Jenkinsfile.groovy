52e3e3ec5e1509febb2992082a5711762cacac62
pantoska-tieto <peter.antoska@tietoevry.com>
2025-07-28 18:15:28 +0200

    Fix shell commands
    * Revert the sudo commands

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,10 +9,10 @@ node {
         def cmd = "${commonCI.DEVOPS_CLONE_CMD}"
         sh "cd ${commonCI.BUILD_DIR} && bash -c \"${cmd}\""
         // Install Application into Zephyr basic installation
-        sh "sudo cd ${commonCI.CI_BUILD_DIR}"
-        sh "sudo cd .venv && sudo source bin/activate && cd .."
+        sh "cd ${commonCI.CI_BUILD_DIR}"
+        sh "cd .venv && source bin/activate && cd .."
         sh "west init -m ${GIT_URL_ZEPH_APP} --mr main customer-application1"
-        sh "sudo cd customer-application1 && west update"
+        sh "cd customer-application1 && west update"
     }
     stage("Test") {
         echo "Testing Zephyr RTOS application..."
